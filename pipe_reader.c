fb32c8677c30d256d54e327c4fe27f84b929d5fc
Ryan Fox <foxryan@gmail.com>
2011-03-08 22:38:24 -0500

    Pure Data is a little bit saner than I gave it credit for. Instead of parsing an 'anything', I can register methods.

--- pipe_reader.c
+++ pipe_reader.c
@@ -14,31 +14,6 @@ void pipe_reader_bang(t_pipe_reader* x)
 	}
 }
 
-void pipe_reader_message(t_pipe_reader* x, t_symbol* s, int argc, t_atom* argv)
-{
-	if(s == gensym("open"))
-	{
-		if(argc > 0)
-		{
-			char filename[256];
-			atom_string(argv,filename,sizeof(filename));
-			pipe_reader_open_file(x,filename);
-		}
-		else
-		{
-			post("No filename given.");
-		}
-	}
-	else if(s == gensym("close"))
-	{
-		pipe_reader_close_file(x);
-	}
-	else
-	{
-		// Ignore unexpected messages.
-	}
-}
-
 void* pipe_reader_new(void)
 {
 	t_pipe_reader* x = (t_pipe_reader*)pd_new(pipe_reader_class);
@@ -61,22 +36,28 @@ void pipe_reader_setup(void)
 	                              sizeof(t_pipe_reader),
 	                              CLASS_DEFAULT, 0);
 	class_addbang(pipe_reader_class, pipe_reader_bang);
-	class_addanything(pipe_reader_class, pipe_reader_message);
+
+	class_addmethod(pipe_reader_class, (t_method)pipe_reader_open_file, gensym("open"), A_DEFSYMBOL, 0);
+	class_addmethod(pipe_reader_class, (t_method)pipe_reader_close_file, gensym("close"), 0);
 }
 
-void pipe_reader_open_file(t_pipe_reader* x, const char* filename)
+void pipe_reader_open_file(t_pipe_reader* x, t_symbol* filename)
 {
 	char buf[256];
-	snprintf(buf,sizeof(buf),"Opening %s.",filename);
+	snprintf(buf,sizeof(buf),"Opening %s.",filename->s_name);
 	post(buf);
+
+	if(x->pipe)
+	{
+		pipe_reader_close_file(x);
+	}
 			
-	x->pipe = fopen(filename,"r");
+	x->pipe = fopen(filename->s_name,"r");
 	
 	if(!x->pipe)
 	{
-		snprintf(buf,sizeof(buf),"Couldn't open %s.",filename);
+		snprintf(buf,sizeof(buf),"Couldn't open %s.",filename->s_name);
 		post(buf);
-		return;
 	}
 }
 
--- pipe_reader.h
+++ pipe_reader.h
@@ -17,7 +17,6 @@ void* pipe_reader_new(void);
 void pipe_reader_free(t_pipe_reader* x);
 
 void pipe_reader_bang(t_pipe_reader* x);
-void pipe_reader_message(t_pipe_reader* x, t_symbol* s, int argc, t_atom* argv);
 
-void pipe_reader_open_file(t_pipe_reader* x, const char* filename);
+void pipe_reader_open_file(t_pipe_reader* x, t_symbol* filename);
 void pipe_reader_close_file(t_pipe_reader* x);
